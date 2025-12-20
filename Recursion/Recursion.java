f07201c0317948a35ffa135f1096d7ec5792b8ab
satyam-kalihari <satyamkalihari17@gmail.com>
2025-12-20 20:00:52 +0530

    print 1 to n

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -11,8 +11,21 @@ public class Recursion {
         System.out.print(" " +n);
         printNTo1(n-1);
     }
+
+    public static void print1ToN(int n){
+        if (n == 1) {
+            System.out.print(" "+1);
+            return;
+        }
+
+        print1ToN(n-1);
+        System.out.print(" " +n);
+        return;
+    }
     
     public static void main(String args[]){
         printNTo1(10);
+        System.out.println();
+        print1ToN(10);
     }
 }

f8a657e1828a3e84d13e3b7a14c2e7d8824941c4
satyam-kalihari <satyamkalihari17@gmail.com>
2025-12-21 12:57:52 +0530

    fibonacci of nth term

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class Fibonacci {
 
-    public static void fibonacchi(int n, int curr, int prev){
+    public static void fibonacci(int n, int curr, int prev){
 
         if (curr > n) {
             return;
@@ -10,14 +10,28 @@ public class Fibonacci {
 
         System.out.print(curr + " ");
         int next = curr+prev;
-        fibonacchi(n, next , curr);
+        fibonacci(n, next , curr);
         return;
+    }
+
+    //fibonacci of nth term
+    public static int fibonacci(int n){
+
+        if (n == 1 || n == 0) {
+            return n;
+        }
+        int fib1 = fibonacci(n-1);
+        int fib2 = fibonacci(n-2);
+        int fn = fib1+fib2;
+        return fn;
     }
 
     public static void main(String[] args) {
         
         int prev = 0;
         System.out.print(prev + " ");
-        fibonacchi(25, prev+1, prev);
+        fibonacci(25, prev+1, prev);
+        System.out.println();
+        System.out.println(fibonacci(25));
     }
 }

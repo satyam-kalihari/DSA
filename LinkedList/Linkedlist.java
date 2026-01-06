e63e3a9f78362a0606c1b4064a4b3f2b900e76bc
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-06 21:31:44 +0530

    Slow-fast concept for finding the mid-node.

--- LinkedList/Linkedlist.java
+++ LinkedList/Linkedlist.java
@@ -53,4 +53,79 @@ public class Linkedlist {
         temp.next = newNode;
 
     }
+
+
+    //Find the middle
+    public Node findMiddleNode(Node head){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow  = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public boolean checkPalindrome(){
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        //Find middle
+        Node midNode = findMiddleNode(head);
+        
+        //reverse the right side of the node
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        //check both the side
+        Node riNode = prev;
+        Node lNode = head;
+        while (riNode != null) {
+            if (lNode.data != riNode.data) {
+                return false;
+            }
+            riNode = riNode.next;
+            lNode = lNode.next;
+        }
+
+        return true;
+    }
+
+    public void printLinkedList(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(1);
+
+        ll.printLinkedList();
+        if (ll.checkPalindrome()) {
+            System.out.println("It is palindrome");
+        }else{
+            System.out.println("Not a palindrome");
+        }
+    }
+
+
 }

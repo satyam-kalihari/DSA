fb37e65bfa776a1459504590aed10542b5a5c589
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-14 19:37:57 +0530

    validate the parentheses string

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    
+    public static boolean validateParentheses(String str){
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+             if (curr == '(' || curr == '[' || curr == '{') {
+                s.push(curr);
+             }
+             else{
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if ((s.peek() == '(' && curr == ')') || (s.peek() == '{' && curr == '}') || (s.peek() == '[' && curr == ']')) {
+                    s.pop();                    
+                }
+                else{
+                    return false;
+                }
+             }
+        }
+
+        if (s.isEmpty()) {
+            return true;
+        }else return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "({[])";
+
+        System.out.println(validateParentheses(str));
+    }
+}

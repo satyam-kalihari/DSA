199dd6db2633c6ee360c0abfb39763786ed7e83d
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-19 08:06:40 +0530

    Queue: Queue reversal

--- Queue/Interleave.java
+++ Queue/Interleave.java
@@ -2,6 +2,7 @@ package Queue;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Interleave {
 
@@ -19,6 +20,17 @@ public class Interleave {
         }
     }
     
+    public static void queueReversal(Queue<Integer> q){
+
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
@@ -32,7 +44,8 @@ public class Interleave {
         q.add(9);
         q.add(10);
 
-        interleaveTwoHalves(q);
+        // interleaveTwoHalves(q);
+        queueReversal(q);
         System.out.println(q);
 
     }

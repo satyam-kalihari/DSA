1335a9637a419ed2dabc26dcdecec3bf6c57abf1
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-20 08:11:39 +0530

    Greedy Algo: Find max number of activities (given end is not-sorted).

--- GreedyAlgo/MaximumActivity.java
+++ GreedyAlgo/MaximumActivity.java
@@ -1,6 +1,9 @@
 package GreedyAlgo;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MaximumActivity {
     
@@ -8,16 +11,27 @@ public class MaximumActivity {
         int[] start = {10, 12, 30};
         int[] end = {20, 25, 30};
 
+        //if end not sorted
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        //sort using lambda function
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         ArrayList<Integer> ans = new ArrayList<>();
         int maxAct = 1;
-        ans.add(0);
+        ans.add(activities[0][0]);
 
-        int lastEnd = end[0];
+        int lastEnd = activities[0][2];
         for (int i = 1; i < end.length; i++) {
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 

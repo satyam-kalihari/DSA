87dad2cbcaf09a0d37086786b87ec7c57f160646
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-27 18:34:57 +0530

    Heap: Remove from the heap.

--- Heap/HeapImp.java
+++ Heap/HeapImp.java
@@ -26,6 +26,44 @@ public class HeapImp {
         }
     }
 
+    public static void heapify(int i, ArrayList<Integer> arr){
+        int left = i*2 + 1;
+        int right = i*2 + 2;
+        int minIdx = i;
+
+        if (left < arr.size() && arr.get(i) > arr.get(left)) {
+            minIdx = left;
+        }
+        if (right < arr.size() && arr.get(i) > arr.get(right)) {
+            minIdx = right;
+        }
+
+        if (minIdx != i) {
+            int temp = arr.get(i);
+            arr.set(i, arr.get(minIdx));
+            arr.set(minIdx, temp);
+
+            heapify(minIdx, arr);
+        }
+    }
+
+    public static int  remove(ArrayList<Integer> arr){
+        int data = arr.get(0);
+
+        //swap
+        int temp = data;
+        arr.set(0, arr.get(arr.size()-1));
+        arr.set(arr.size()-1, temp);
+
+        //remove the last element
+        arr.remove(arr.size()-1);
+
+        //fix the heap
+        heapify(0, arr);
+
+        return data;
+    }
+
     public static void main(String[] args) {
         Heap objHeap = new Heap();
         objHeap.add(2);
@@ -36,5 +74,9 @@ public class HeapImp {
         System.out.println(objHeap.arr);
         objHeap.add(1);
         System.out.println(objHeap.arr);
+        remove(objHeap.arr);
+        System.out.println(objHeap.arr);
+
+
     }
 }

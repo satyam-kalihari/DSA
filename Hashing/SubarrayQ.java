41321ce031613c5514276d71dfb47d843a1a5423
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-31 17:09:26 +0530

    HashMap: Find the number of subarray with sum k.

--- Hashing/SubarrayQ.java
+++ Hashing/SubarrayQ.java
@@ -20,17 +20,26 @@ public class SubarrayQ {
         return len;
     }
 
-    // public static int subarraySumK(int[] array, int k){
-    //     HashMap<Integer, Integer> sc = new HashMap<>();
-    //     int sum = 0;
-    //     int count = 0;
-    //     return count;
-    // }
+    public static int subarraySumK(int[] array, int k){
+        HashMap<Integer, Integer> sc = new HashMap<>();
+        int sum = 0;
+        int ans = 0;
+        sc.put(0, 1);
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+            if (sc.containsKey(sum - k)) {
+                ans += sc.get(sum-k);
+            }
+            sc.put(sum, sc.getOrDefault(sum, 0)+1);
+        }
+        return ans;
+    }
     
     public static void main(String[] args) {
-        int[] array = {15, -2, 2, -8, 1, 7, 10, 23};
+        int[] array = {10, 2, -2, -20, 10};
         int LSWS0 = subarraySum0(array);//Largest subarray with sum 0;
-        // int NSSEK = subarraySumK(array, 3);
+        int NSSEK = subarraySumK(array, -10);
         System.out.println(LSWS0);
+        System.out.println(NSSEK);
     }
 }

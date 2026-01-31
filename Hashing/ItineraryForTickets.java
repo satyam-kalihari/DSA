1ffff6072351c23080d980941dbe7eda81c290ef
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-31 16:02:00 +0530

    HashMap: Itinerary for Tickets.

---
+++ Hashing/ItineraryForTickets.java
@@ -0,0 +1,35 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class ItineraryForTickets {
+
+    public static String getStart(HashMap<String, String> tickets){
+
+        HashMap<String, String> reverse = new HashMap<>();
+        for (String key : tickets.keySet()) {
+            reverse.put(tickets.get(key), key);
+        }
+        for (String key : tickets.keySet()) {
+            if (!reverse.containsKey(key)) {
+                return key;
+            }
+        }
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Delhi", "Goa");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Chennai", "Bengaluru");
+
+        String start = getStart(tickets);
+        System.out.print(start);
+        for (String key : tickets.keySet()) {
+            System.out.print(" -> " + tickets.get(start));
+            start = tickets.get(start);
+        }
+    }
+}

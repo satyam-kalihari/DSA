07d13d2247f5440c3bf6531dfc607c47576d8b5b
satyam-kalihari <satyamkalihari17@gmail.com>
2026-02-03 06:34:25 +0530

    Tries: creation & insertion.

---
+++ Tries/TriesImp.java
@@ -0,0 +1,34 @@
+package Tries;
+
+public class TriesImp {
+    public static class Node {
+        Node[] children = new Node[26];
+        boolean endOfWord = false;
+
+        Node(){
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i)-'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+                curr = curr.children[idx];
+            }
+        }
+        curr.endOfWord = true;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"the", "a", "there", "their", "any", "three"};
+        for (String string : str) {
+            insert(string);
+        }
+    }
+}

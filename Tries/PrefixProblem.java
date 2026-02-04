475011c8eab88bac16d00d50fd3ea88b45ea0eb3
satyam-kalihari <satyamkalihari17@gmail.com>
2026-02-04 07:30:20 +0530

    Tries: Find the unique prefix for each word.

---
+++ Tries/PrefixProblem.java
@@ -0,0 +1,56 @@
+package Tries;
+
+public class PrefixProblem {
+    public static class Node {
+    
+        Node[] children = new Node[26];
+        boolean eow = false;
+        int feq = 0;
+        Node(){
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i)-'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr.children[idx].feq++;
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static StringBuilder searchPrifix(String word){
+        Node curr = root;
+        StringBuilder prefix = new StringBuilder();
+        for(int i = 0; i < word.length(); i++){
+            int idx = word.charAt(i) - 'a';
+            curr = curr.children[idx];
+            prefix.append(word.charAt(i));
+            if (curr.feq == 1) {
+               break; 
+            }
+        }
+        
+        return prefix;
+    }
+    public static void printPrifix(String[] str){
+        for (String word : str) {
+            System.out.println(searchPrifix(word));
+        }
+    }
+    public static void main(String[] args) {
+        String[] str = {"zebra", "dog", "duck", "dove"};
+        for (String s : str) {
+            insert(s);
+        }
+        printPrifix(str);
+    }
+}

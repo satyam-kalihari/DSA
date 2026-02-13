add73ed1d99a05d1ee64a25ccf83fc78b52b2a3f
satyam-kalihari <satyamkalihari17@gmail.com>
2026-02-13 07:19:58 +0530

    Graphs: DFS & BFS for connected components

---
+++ Graphs/LectureII.java
@@ -0,0 +1,101 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LectureII {
+    public static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph){
+        boolean[] vis = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                bfsUtil(graph, vis);
+            }
+        }
+    }
+
+    public static void bfsUtil(ArrayList<Edge>[] graph, boolean[] vis){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph){
+        boolean[] vis = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                dfaUtil(graph, i, vis);
+            }
+        }
+    }
+
+    public static void dfaUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis){
+
+        System.out.print(curr + " ");
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfaUtil(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 5;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        //vertex 0
+        graph[0].add(new Edge(0, 1, 5));
+
+        //vertex 1
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        //vertex 2
+        graph[2].add(new Edge(2, 1, 0));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        //vertex 3
+        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 3));
+
+        //vertex 0
+        graph[4].add(new Edge(4, 2, 2));
+
+        //neighbours of 2
+        // for (Edge nei : graph[2]) {
+        //     System.out.println(nei.dest);
+        // }
+        // bfs(graph);
+        dfs(graph);
+    }
+}

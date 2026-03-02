76217b9f7ad02119c6813032ba70cafcb6029048
satyam-kalihari <satyamkalihari17@gmail.com>
2026-03-02 13:52:33 +0530

    DP: 0-1 knapsack using Tabulation.

--- DynamicProgramming/knapsack/zeroOneKnapsack.java
+++ DynamicProgramming/knapsack/zeroOneKnapsack.java
@@ -23,6 +23,36 @@ public class zeroOneKnapsack {
         }
     }
 
+    public static int tabulationKnap(int val[], int wt[], int W){
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
+
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 0; i < dp[0].length; i++){
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                int v = val[i-1];
+                int w = wt[i-1];
+                if (w<=j) {
+                    int incProfit = v + dp[i-1][j-w];
+                    int exeProfit = dp[i-1][j];
+
+                    dp[i][j] = Math.max(incProfit, exeProfit);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         int[] val = {15, 14, 10, 45, 30};
         int[] wt = {2, 5, 1, 3, 4};
@@ -34,6 +64,7 @@ public class zeroOneKnapsack {
             }
         }
 
-        System.out.println(memoizationKnap(val, wt, W, val.length, dp));
+        // System.out.println(memoizationKnap(val, wt, W, val.length, dp));
+        System.out.println(tabulationKnap(val, wt, W));
     }
 }
